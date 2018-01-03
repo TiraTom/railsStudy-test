85e0f902eea5da341f90bd83d5d2ccce2ec4028b
Tira <217bssr@gmail.com>
2018-01-03 12:30:07 +0000

    フィルサイズのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,13 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true, on: :create
   validates :image, presence: true
   validates :profile, presence: true
+  validate :picture_size
+
+  private
+  def picture_size
+    if image.size > 500.kilobytes
+      errors.add(:image, "は500KB以内のデータを選択してください")
+    end
+  end
 
 end
